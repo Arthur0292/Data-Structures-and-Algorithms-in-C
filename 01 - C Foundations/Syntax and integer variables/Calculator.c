3aeb188fa8a16be3d2b3c96717c06bdb63dc28b6
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-08-15 11:12:00 -0300

    Update Calculator.c

--- 01 - C Foundations/Syntax and integer variables/Calculator.c
+++ 01 - C Foundations/Syntax and integer variables/Calculator.c
@@ -25,21 +25,23 @@ int main() {
 
     int opcao, a , b;
     scanf("%d", &opcao);
+
+    while(opcao != 5){
+        if(opcao == 1){
+            scanf("%d %d", &a, &b);
+            printf("%d + %d = %d\n", a, b, soma(a, b));
+        }else if(opcao == 2){
+            scanf("%d %d", &a, &b);
+            printf("%d - %d = %d\n", a, b, subtracao(a, b));
+        }else if(opcao == 3){
+            scanf("%d %d", &a, &b);
+            printf("%d * %d = %d\n", a, b, multiplicao(a, b));
+        }else if(opcao == 4){
+            scanf("%d %d", &a, &b);
+            printf("%d / %d = %d\n", a, b, divisao(a, b));
+        }
     
-    if(opcao == 1){
-        scanf("%d %d", &a, &b);
-        printf("%d + %d = %d\n", a, b, soma(a, b));
-    }else if(opcao == 2){
-        scanf("%d %d", &a, &b);
-        printf("%d - %d = %d\n", a, b, subtracao(a, b));
-    }else if(opcao == 3){
-        scanf("%d %d", &a, &b);
-        printf("%d * %d = %d\n", a, b, multiplicao(a, b));
-    }else if(opcao == 4){
-        scanf("%d %d", &a, &b);
-        printf("%d / %d = %d\n", a, b, divisao(a, b));
-    }else if(opcao == 5){
-        return 0;
+        scanf("%d", &opcao);
     }
     
 
