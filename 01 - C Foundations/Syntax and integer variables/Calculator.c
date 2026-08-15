2102e86d4ab4368ab58d1a0afbab93a40df0c32e
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-08-14 21:54:25 -0300

    Add basic calculator functions in Calculator.c

---
+++ 01 - C Foundations/Syntax and integer variables/Calculator.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int soma(int x, int y){
+    int resultado = x + y;
+    return resultado;
+}
+
+int subtracao(int x, int y){
+    int resultado = x - y;
+    return resultado;
+}
+
+int multiplicao(int x, int y){
+    int resultado = x * y;
+    return resultado;
+}
+
+int divisao(int x, int y){
+    int resultado = x / y;
+    return resultado;
+}
+
+
+int main() {
+
+    int opcao, a , b;
+    scanf("%d", &opcao);
+    
+    if(opcao == 1){
+        scanf("%d %d", &a, &b);
+        printf("%d + %d = %d\n", a, b, soma(a, b));
+    }else if(opcao == 2){
+        scanf("%d %d", &a, &b);
+        printf("%d - %d = %d\n", a, b, subtracao(a, b));
+    }else if(opcao == 3){
+        scanf("%d %d", &a, &b);
+        printf("%d * %d = %d\n", a, b, multiplicao(a, b));
+    }else if(opcao == 4){
+        scanf("%d %d", &a, &b);
+        printf("%d / %d = %d\n", a, b, divisao(a, b));
+    }else if(opcao == 5){
+        return 0;
+    }
+    
+
+    return 0;
+}
